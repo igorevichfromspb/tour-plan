a9fcee2d230275bdff6110e22f6e864b16caa830
Ivan Matveev <igorevichwork@gmail.com>
2021-09-25 20:26:39 +0300

    create: added responsive for reviews

--- js/main.js
+++ js/main.js
@@ -18,3 +18,18 @@ const swiper = new Swiper(".swiper", {
   },
   effect: "coverflow",
 });
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
