7b0843c36e0a1eb66cafae6e667faeb1c52830d3
Ivan Matveev <igorevichwork@gmail.com>
2021-10-04 16:45:25 +0300

    create: added PHPMailer for newsletter

--- send.php
+++ send.php
@@ -8,6 +8,7 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$email = $_POST['email'];
 
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
@@ -16,6 +17,7 @@ $body = "
 <b>Имя:</b> $name<br>
 <b>Телефон:</b> $phone<br><br>
 <b>Сообщение:</b><br>$message
+<b>Почта:</b> $email<br><br>
 ";
 
 // Настройки PHPMailer
