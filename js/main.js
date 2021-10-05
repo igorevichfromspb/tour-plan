701638fea8c2666e47b99ab07085d8bc97266561
Ivan Matveev <igorevichwork@gmail.com>
2021-10-06 00:26:15 +0300

    fix: responsive for footer

--- js/main.js
+++ js/main.js
@@ -1,38 +1,60 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button--next",
-    prevEl: ".swiper-button--prev",
-  },
-  // keyboard: {
-  //   enabled: true,
-  //   onlyInViewport: false,
-  // },
-  // autoplay: {
-  //   delay: 3000,
-  //   pauseOnMouseEnter: true,
-  //   disableOnInteraction: false,
-  // },
-  // effect: "coverflow",
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button--next",
+      prevEl: ".swiper-button--prev",
+    },
+    // keyboard: {
+    //   enabled: true,
+    //   onlyInViewport: false,
+    // },
+    // autoplay: {
+    //   delay: 3000,
+    //   pauseOnMouseEnter: true,
+    //   disableOnInteraction: false,
+    // },
+    // effect: "coverflow",
+  });
 
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
 
-let menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+  let menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+
+  let modalButton = $("[data-toggle=modal]");
+  let closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    let modalOverley = $(".modal__overley");
+    let modalDialog = $(".modal__dialog");
+    modalOverley.addClass("modal__overley--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    let modalOverley = $(".modal__overley");
+    let modalDialog = $(".modal__dialog");
+    modalOverley.removeClass("modal__overley--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
