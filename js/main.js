388a55aaa552c30b1e3d6100d16731c860016377
Ivan Matveev <igorevichwork@gmail.com>
2021-10-08 16:25:10 +0300

    create: validation

--- js/main.js
+++ js/main.js
@@ -57,4 +57,45 @@ $(document).ready(function () {
     modalOverley.removeClass("modal__overley--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  // Обработка форм
+  $(".modal__form").validate({
+    errorClass: "form-1",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "The name must be at least 2 letters long",
+      },
+      email: {
+        required: "We need your email to contact you",
+        email: "Еmail must be in the format of name@domain.com",
+      },
+      phone: {
+        required: "Phone number required",
+      },
+    },
+  });
+
+  $(".newsletter__subscribe").validate({
+    errorClass: "form-2",
+    messages: {
+      email: {
+        required: "We need your email to contact you",
+        email: "Еmail must be in the format of name@domain.com",
+      },
+    },
+  });
+
+  $(".footer__form").validate({
+    errorClass: "form-3",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "The name must be at least 2 letters long",
+      },
+      phone: {
+        required: "Phone number required",
+      },
+    },
+  });
+  $(".phone").mask("+7 (000) 000-0000");
 });
